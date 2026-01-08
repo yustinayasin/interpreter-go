3599d21b2e7f7e8a0b597cd0937f3269a5dcae15
Yustina Yasin <yustinayasin@Yustinas-MacBook-Pro.local>
2026-01-08 07:11:42 +0700

    add: parser, error message

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,27 +8,25 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-			let x = 5;
-			let y = 10;
-			let foobar = 838383;
+			let x 5;
+			let = 10;
+			let 838383;
 		`
 
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
 
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-			len(program.Statements))
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
 	}
 
-	tests := []struct {
-		expectedIdentifier string
-	}{
+	tests := []struct{ expectedIdentifier string }{
 		{"x"},
 		{"y"},
 		{"foobar"},
@@ -67,4 +65,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
